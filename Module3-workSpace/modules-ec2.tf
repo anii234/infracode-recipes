db1a8f5074128c1600d5b3d8f7038e4f4e75d344
Anubhav Bhardwaj <37350430+anii234@users.noreply.github.com>
2024-08-16 02:00:45 +0530

    Update modules-ec2.tf

--- Module3-workSpace/modules-ec2.tf
+++ Module3-workSpace/modules-ec2.tf
@@ -1,9 +1,5 @@
 resource "aws_instance" "ec2MAchine" {
     ami = "ami-01b799c439fd5516a"
-    tags = {
-      Name = var.serverType[count.index]
-    }
-    count = 3
     instance_type = "t2.micro"
   
 }
